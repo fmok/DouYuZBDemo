93f0822116f56ac0378ea01d472a53c9e32a2aff
fm <wangjiuyin@jiemian.com>
2017-07-12 15:55:13 +0800

    add storyBoard

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by fm on 2017/7/12.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Follow")
+        addChildVC(storyName: "Profile")
+    }
+    
+    private func addChildVC(storyName : String) {
+        // 1、通过storyBoard获取控制器
+        let childVC = UIStoryboard(name: storyName, bundle:nil).instantiateInitialViewController()!
+        // 2、将childVC作为子控制器
+        addChildViewController(childVC)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
