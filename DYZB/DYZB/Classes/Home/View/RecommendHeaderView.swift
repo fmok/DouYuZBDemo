f2d33d87964315f7638270bd08713a727ad58aff
fm <wangjiuyin@jiemian.com>
2017-07-17 18:08:50 +0800

    update

---
+++ DYZB/DYZB/Classes/Home/View/RecommendHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendHeaderView.swift
+//  DYZB
+//
+//  Created by fm on 2017/7/17.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+import UIKit
+
+class RecommendHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
